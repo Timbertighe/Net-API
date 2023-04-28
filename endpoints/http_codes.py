11dfa000da45a01e66d9f3a9d4f5f6bf0562f4e9
LukeRoberson <130713342+LukeRoberson@users.noreply.github.com>
2023-04-28 15:30:07 +1000

    Added more codes

--- endpoints/http_codes.py
+++ endpoints/http_codes.py
@@ -21,8 +21,26 @@ Misc Variables:
 
     HTTP_OK : int
         The '200 - OK' return code
+        https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/200
+    HTTP_CREATED : int
+        The '201 - Created' code
+        https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/201
+    HTTP_ACCEPTED : int
+        The '202 - Accepted' code
+        https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/202
+    HTTP_NOCONTENT : int
+        The '204 - No content' code
+        https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/204
     HTTP_BADREQUEST : int
         The '400 - Bad Request' return code
+        https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/400
+    HTTP_NOTFOUND : int
+        The '404 - Not found' code
+        https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/404
+    HTTP_CONFLICT : int
+        The '409 - Conflict' code
+        https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/409
+
 
 Author:
     Luke Robertson - April 2023
@@ -30,4 +48,8 @@ Author:
 
 HTTP_OK = 200
 HTTP_CREATED = 201
+HTTP_ACCEPTED = 202
+HTTP_NOCONTENT = 204
 HTTP_BADREQUEST = 400
+HTTP_NOTFOUND = 404
+HTTP_CONFLICT = 409
