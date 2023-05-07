4b9b89a2f56e7181bcd7d34fef4f3e13229fc6e8
LukeRoberson <130713342+LukeRoberson@users.noreply.github.com>
2023-05-07 15:48:01 +1000

    Converted to class-based

--- endpoints/interfaces.py
+++ endpoints/interfaces.py
@@ -3,20 +3,16 @@ Interface information
 
 Modules:
     3rd Party: None
-    Internal: http_codes
+    Internal: http_codes, api
 
 Classes:
 
-    None
+    Interfaces
+        Represents the /devices/:device_id/interfaces endpoint
 
 Functions
 
-    get_interfaces
-        Gets interface information
-    patch_interfaces
-        Updates interface configuration
-    post_interfaces_op
-        Performs operational tasks on an interface
+    None
 
 Exceptions:
 
@@ -27,212 +23,240 @@ Misc Variables:
     None
 
 Author:
-    Luke Robertson - April 2023
+    Luke Robertson - May 2023
 """
 
 import endpoints.http_codes as http_codes
+import endpoints.api as api
 
 
-def get_interfaces(device_id, interface, summary):
+class Interfaces(api.ApiCall):
     '''
-    Handle a GET request to the /devices/:device_id/interfaces endpoint
+    Create an object to represent the /devices/:device_id/interfaces endpoint
 
-    Parameters:
-        device_id : str
-            The ID of the device
-        interface : str
-            A specific interface to query
-        summary : bool
-            True if we want just a simple summary, not all details
+    Supports being instantiated with the 'with' statement
 
-    Raises:
-        None
+    Attributes
+    ----------
+    request : flask.request
+        The request object from Flask
+    device_id : str
+        The device ID to query
 
-    Returns:
-        response : JSON
-            The JSON response with the requested information or error
-            For a POST, this echoes back the request body
-        code : int
-            The HTTP response code
+    Methods
+    -------
+    get()
+        Handle a GET request to the /devices/:device_id/interfaces endpoint
+    patch()
+        Handle a PATCH request to the /devices/:device_id/interfaces endpoint
+    post()
+        Handle a POST request to the /devices/:device_id/interfaces/op endpoint
     '''
 
-    # Check if we want a specific interface
-    if interface:
-        pass
+    def __init__(self, request, device_id):
+        '''
+        Class constructor
 
-    # Check if we want a summary only
-    if summary:
-        pass
+        Parameters:
+            request : flask.request
+                The request object from Flask
+            device_id : str
+                The device ID to query
 
-    # Build the response
-    response = {
-        "interfaces": [
-            {
-                "name": "ge-0/0/0",
-                "mac": "4c:6d:58:00:00:00",
-                "description": "Workstations",
-                "family": "",
-                "address": "",
-                "native_vlan": 1,
-                "speed": 1000,
-                "counters": {
-                    "bps_in": 550800,
-                    "bps_out": 682184,
-                    "bytes_in": 4755699005,
-                    "bytes_out": 629507153,
-                    "pps_in": 51088,
-                    "pps_out": 74936,
-                    "packets_in": 3979923,
-                    "packets_out": 2173825
-                },
-                "subinterfaces": [
-                    {
-                        "subinterface": "unit 0",
-                        "family": "ethernet",
-                        "address": "204",
-                        "description": "Workstation"
+        Raises:
+            None
+
+        Returns:
+            None
+        '''
+
+        # Call the superclass constructor
+        super().__init__(request)
+
+        # Store the device ID
+        self.device_id = device_id
+
+        # Extract parameters from the request
+        self.interface = False
+        self.summary = False
+
+        # Check for the 'interface' parameter
+        if 'interface' in self.args:
+            self.interface = self.args.getlist('interface')
+
+        # Check for the 'summary' parameter
+        if 'summary' in self.args:
+            self.summary = self.args.getlist('summary')
+
+    def get(self):
+        '''
+        Handle a GET request to the /devices/:device_id/interfaces endpoint
+
+        Parameters:
+            None
+
+        Raises:
+            None
+
+        Returns:
+            None
+        '''
+
+        # Check if we want a specific interface
+        if self.interface:
+            pass
+
+        # Check if we want a summary only
+        if self.summary:
+            pass
+
+        # Build the response
+        self.response = {
+            "interfaces": [
+                {
+                    "name": "ge-0/0/0",
+                    "mac": "4c:6d:58:00:00:00",
+                    "description": "Workstations",
+                    "family": "",
+                    "address": "",
+                    "native_vlan": 1,
+                    "speed": 1000,
+                    "counters": {
+                        "bps_in": 550800,
+                        "bps_out": 682184,
+                        "bytes_in": 4755699005,
+                        "bytes_out": 629507153,
+                        "pps_in": 51088,
+                        "pps_out": 74936,
+                        "packets_in": 3979923,
+                        "packets_out": 2173825
+                    },
+                    "subinterfaces": [
+                        {
+                            "subinterface": "unit 0",
+                            "family": "ethernet",
+                            "address": "204",
+                            "description": "Workstation"
+                        }
+                    ],
+                    "poe": {
+                        "admin": True,
+                        "operational": True,
+                        "max": 15.4,
+                        "used": 11.8
                     }
-                ],
-                "poe": {
-                    "admin": True,
-                    "operational": True,
-                    "max": 15.4,
-                    "used": 11.8
                 }
-            }
-        ]
-    }
+            ]
+        }
 
-    code = http_codes.HTTP_OK
+        self.code = http_codes.HTTP_OK
 
-    return response, code
+    def patch(self):
+        '''
+        Handle a PATCH request to the /devices/:device_id/interfaces endpoint
 
+        Parameters:
+            None
 
-def patch_interfaces_op(device_id, body):
-    '''
-    Handle a PATCH request to the /devices/:device_id/interfaces endpoint
+        Raises:
+            None
 
-    Parameters:
-        device_id : str
-            The ID of the device
-        body : dict
-            The operational request
+        Returns:
+            None
+        '''
 
-    Raises:
-        None
-
-    Returns:
-        response : JSON
-            The JSON response with the requested information or error
-            For a POST, this echoes back the request body
-        code : int
-            The HTTP response code
-    '''
-
-    # Build the response
-    response = {
-        "interfaces": [
-            {
-                "name": "ge-0/0/0",
-                "mac": "4c:6d:58:00:00:00",
-                "description": "Workstations",
-                "family": "",
-                "address": "",
-                "native_vlan": 1,
-                "speed": 1000,
-                "counters": {
-                    "bps_in": 550800,
-                    "bps_out": 682184,
-                    "bytes_in": 4755699005,
-                    "bytes_out": 629507153,
-                    "pps_in": 51088,
-                    "pps_out": 74936,
-                    "packets_in": 3979923,
-                    "packets_out": 2173825
-                },
-                "subinterfaces": [
-                    {
-                        "subinterface": "unit 0",
-                        "family": "ethernet",
-                        "address": "204",
-                        "description": "Workstation"
+        # Build the response
+        self.response = {
+            "interfaces": [
+                {
+                    "name": "ge-0/0/0",
+                    "mac": "4c:6d:58:00:00:00",
+                    "description": "Workstations",
+                    "family": "",
+                    "address": "",
+                    "native_vlan": 1,
+                    "speed": 1000,
+                    "counters": {
+                        "bps_in": 550800,
+                        "bps_out": 682184,
+                        "bytes_in": 4755699005,
+                        "bytes_out": 629507153,
+                        "pps_in": 51088,
+                        "pps_out": 74936,
+                        "packets_in": 3979923,
+                        "packets_out": 2173825
+                    },
+                    "subinterfaces": [
+                        {
+                            "subinterface": "unit 0",
+                            "family": "ethernet",
+                            "address": "204",
+                            "description": "Workstation"
+                        }
+                    ],
+                    "poe": {
+                        "admin": True,
+                        "operational": True,
+                        "max": 15.4,
+                        "used": 11.8
                     }
-                ],
-                "poe": {
-                    "admin": True,
-                    "operational": True,
-                    "max": 15.4,
-                    "used": 11.8
                 }
-            }
-        ]
-    }
+            ]
+        }
 
-    code = http_codes.HTTP_CREATED
+        self.code = http_codes.HTTP_CREATED
 
-    return response, code
+    def post(self):
+        '''
+        Handle a POST request to the /devices/:device_id/interfaces/op endpoint
 
+        Parameters:
+            None
 
-def post_interfaces_op(device_id, body):
-    '''
-    Handle a POST request to the /devices/:device_id/interfaces/op endpoint
+        Raises:
+            None
 
-    Parameters:
-        device_id : str
-            The ID of the device
-        body : dict
-            The operational request
+        Returns:
+            None
+        '''
 
-    Raises:
-        None
-
-    Returns:
-        response : JSON
-            The JSON response with the requested information or error
-            For a POST, this echoes back the request body
-        code : int
-            The HTTP response code
-    '''
-
-    # Build the response
-    response = {
-        "interfaces": [
-            {
-                "name": "ge-0/0/0",
-                "mac": "4c:6d:58:00:00:00",
-                "description": "Workstations",
-                "family": "",
-                "address": "",
-                "native_vlan": 1,
-                "speed": 1000,
-                "counters": {
-                    "bps_in": 550800,
-                    "bps_out": 682184,
-                    "bytes_in": 4755699005,
-                    "bytes_out": 629507153,
-                    "pps_in": 51088,
-                    "pps_out": 74936,
-                    "packets_in": 3979923,
-                    "packets_out": 2173825
-                },
-                "subinterfaces": [
-                    {
-                        "subinterface": "unit 0",
-                        "family": "ethernet",
-                        "address": "204",
-                        "description": "Workstation"
+        # Build the response
+        self.response = {
+            "interfaces": [
+                {
+                    "name": "ge-0/0/0",
+                    "mac": "4c:6d:58:00:00:00",
+                    "description": "Workstations",
+                    "family": "",
+                    "address": "",
+                    "native_vlan": 1,
+                    "speed": 1000,
+                    "counters": {
+                        "bps_in": 550800,
+                        "bps_out": 682184,
+                        "bytes_in": 4755699005,
+                        "bytes_out": 629507153,
+                        "pps_in": 51088,
+                        "pps_out": 74936,
+                        "packets_in": 3979923,
+                        "packets_out": 2173825
+                    },
+                    "subinterfaces": [
+                        {
+                            "subinterface": "unit 0",
+                            "family": "ethernet",
+                            "address": "204",
+                            "description": "Workstation"
+                        }
+                    ],
+                    "poe": {
+                        "admin": True,
+                        "operational": True,
+                        "max": 15.4,
+                        "used": 11.8
                     }
-                ],
-                "poe": {
-                    "admin": True,
-                    "operational": True,
-                    "max": 15.4,
-                    "used": 11.8
                 }
-            }
-        ]
-    }
+            ]
+        }
 
-    code = http_codes.HTTP_OK
-
-    return response, code
+        self.code = http_codes.HTTP_OK
