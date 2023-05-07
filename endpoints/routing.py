b002adcd0fae6d76412fc319b591dfde621ed7a0
LukeRoberson <130713342+LukeRoberson@users.noreply.github.com>
2023-05-07 15:49:45 +1000

    Converted to class; Consolidated with OSPF

--- endpoints/routing.py
+++ endpoints/routing.py
@@ -1,18 +1,20 @@
 """
-Routing table
+Manage routing functions (eg, routing table, OSPF)
 
 Modules:
     3rd Party: None
-    Internal: http_codes
+    Internal: http_codes, api
 
 Classes:
 
-    None
+    Routing_Table
+        Represents the /devices/:device_id/routing_table endpoint
+    Ospf
+        Represents the /devices/:device_id/ospf endpoint
 
 Functions
 
-    get_routes
-        Gets routing table entries
+    None
 
 Exceptions:
 
@@ -23,55 +25,243 @@ Misc Variables:
     None
 
 Author:
-    Luke Robertson - April 2023
+    Luke Robertson - May 2023
 """
 
 import endpoints.http_codes as http_codes
+import endpoints.api as api
 
 
-def get_routes(device_id, route):
+class Routing_Table(api.ApiCall):
     '''
-    Handle a GET request to the /devices/:device_id/routing_table endpoint
+    Object to represent the /devices/:device_id/routing_table endpoint
 
-    Parameters:
-        device_id : str
-            The ID of the device
-        route : str
-            A specific route to search for
+    Supports being instantiated with the 'with' statement
 
-    Raises:
-        None
+    Attributes
+    ----------
+    request : flask.request
+        The request object from Flask
+    device_id : str
+        The device ID to query
 
-    Returns:
-        response : JSON
-            The JSON response with the requested information or error
-            For a POST, this echoes back the request body
-        code : int
-            The HTTP response code
+    Methods
+    -------
+    get()
+        Handle a GET request to the /devices/:device_id/routing_table endpoint
     '''
 
-    # Check if we want a specific interface
-    if route:
-        pass
+    def __init__(self, request, device_id):
+        '''
+        Class constructor
 
-    # Build the response
-    response = {
-        "entry": [
-            {
-                "route": " 10.1.1.0/24",
-                "next-hop": [
-                    {
-                        "hop": "10.2.2.2",
-                        "protocol": "Static/5",
-                        "interface": "vlan.29",
-                        "metric": 0,
-                        "active": True
-                    }
-                ]
-            }
-        ]
-    }
+        Parameters:
+            request : flask.request
+                The request object from Flask
+            device_id : str
+                The device ID to query
 
-    code = http_codes.HTTP_OK
+        Raises:
+            None
 
-    return response, code
+        Returns:
+            None
+        '''
+
+        # Call the superclass constructor
+        super().__init__(request)
+
+        # Store the device ID
+        self.device_id = device_id
+
+        # Check for the 'route' parameter
+        self.route = False
+        if 'route' in self.args:
+            self.route = self.args.getlist('route')
+
+    def get(self):
+        '''
+        Handle a GET request to the /devices/:device_id/routing_table endpoint
+
+        Parameters:
+            None
+
+        Raises:
+            None
+
+        Returns:
+            None
+        '''
+
+        # Check if we want a specific interface
+        if self.route:
+            pass
+
+        # Build the response
+        self.response = {
+            "entry": [
+                {
+                    "route": " 10.1.1.0/24",
+                    "next-hop": [
+                        {
+                            "hop": "10.2.2.2",
+                            "protocol": "Static/5",
+                            "interface": "vlan.29",
+                            "metric": 0,
+                            "active": True
+                        }
+                    ]
+                }
+            ]
+        }
+
+        self.code = http_codes.HTTP_OK
+
+
+class Ospf(api.ApiCall):
+    '''
+    Object to represent the /devices/:device_id/ospf endpoint
+
+    Supports being instantiated with the 'with' statement
+
+    Attributes
+    ----------
+    request : flask.request
+        The request object from Flask
+    device_id : str
+        The device ID to query
+
+    Methods
+    -------
+    get()
+        Handle a GET request to the /devices/:device_id/routing_table endpoint
+    '''
+
+    def __init__(self, request, device_id):
+        '''
+        Class constructor
+
+        Parameters:
+            request : flask.request
+                The request object from Flask
+            device_id : str
+                The device ID to query
+
+        Raises:
+            None
+
+        Returns:
+            None
+        '''
+
+        # Call the superclass constructor
+        super().__init__(request)
+
+        # Store the device ID
+        self.device_id = device_id
+
+    def get(self):
+        '''
+        Handle a GET request to the /devices/:device_id/ospf endpoint
+
+        Parameters:
+            None
+
+        Raises:
+            None
+
+        Returns:
+            None
+        '''
+
+        # Build the response
+        self.response = {
+            "id": "10.1.1.1",
+            "reference": "100g",
+            "areas": [
+                {
+                    "id": "0.0.0.10",
+                    "type": "Not Stub",
+                    "auth_type": "None",
+                    "neighbors": 2
+                }
+            ],
+            "neighbor": [
+                {
+                    "address": "172.1.1.1",
+                    "interface": "st0.41",
+                    "state": "Full",
+                    "id": "10.2.2.2",
+                    "area": "0.0.0.10"
+                }
+            ],
+            "interface": [
+                {
+                    "name": "irb.10",
+                    "state": "DRother",
+                    "area": "0.0.0.10",
+                    "neighbors": 0,
+                    "mtu": 9192,
+                    "cost": 8015,
+                    "type": "P2MP",
+                    "mask": "255.255.255.0",
+                    "Auth_type": "None",
+                    "passive": True
+                }
+            ]
+        }
+
+        self.code = http_codes.HTTP_OK
+
+    def post(self):
+        '''
+        Handle a POST request to the /devices/:device_id/ospf/op endpoint
+
+        Parameters:
+            None
+
+        Raises:
+            None
+
+        Returns:
+            None
+        '''
+
+        # Build the response
+        self.response = {
+            "id": "10.1.1.1",
+            "reference": "100g",
+            "areas": [
+                {
+                    "id": "0.0.0.10",
+                    "type": "Not Stub",
+                    "auth_type": "None",
+                    "neighbors": 2
+                }
+            ],
+            "neighbor": [
+                {
+                    "address": "172.2.2.2",
+                    "interface": "st0.41",
+                    "state": "Full",
+                    "id": "10.2.2.2",
+                    "area": "0.0.0.10"
+                }
+            ],
+            "interface": [
+                {
+                    "name": "irb.10",
+                    "state": "DRother",
+                    "area": "0.0.0.10",
+                    "neighbors": 0,
+                    "mtu": 9192,
+                    "cost": 8015,
+                    "type": "P2MP",
+                    "mask": "255.255.255.0",
+                    "Auth_type": "None",
+                    "passive": True
+                }
+            ]
+        }
+
+        self.code = http_codes.HTTP_OK
